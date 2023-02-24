f5236a0f5810184ed7a4017a00b666ebf66d837f
Cassandra <cdembinski@laigledu.net>
2023-02-24 15:03:28 +0100

    modification des ajouts

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,61 @@
+// import React from 'react';
+// import { SiConsul } from 'react-icons/si';
+// import { BsPhoneVibrate } from 'react-icons/Bs';
+// import { AiOutlineGlobal } from 'react-icons/Ai';
+// import { CgMenuGridO } from 'react-icons/Cg';
+
+// ajout du logo avec le bon chemin une fois créer
+// import logo from '../../assets/logo.png';
+
+// const Navbar = () => {
+//   return (
+//     <div className='navBar flex'>
+//         <div className='navBarOne flex'>
+//             <div>
+//                 <SiConsul />
+//             </div>
+
+//             <div className='none flex'>
+//                 <li className='flex'> <BsPhoneVibrate className='icon'/> Support</li>
+//                 <li className='flex'> <AiOutlineGlobal className='icon'/> Languages</li>
+//             </div>
+
+//             <div className="atb flex">
+//                 <span>Sing In</span>
+//                 <span>Sign Out</span>
+//             </div>
+
+//             <div className="navBarTwo">
+//                 <div className="logoDiv">
+{
+  /* <img src={logo} className='Logo /> /}
+                </div>
+            </div>
+
+            <div className="navBarMenu">
+                <ul className="menu flex">
+                    <li className='listItem'>Home</li>
+                    <li className='listItem'>Home</li>
+                    <li className='listItem'>Home</li>
+                    <li className='listItem'>Home</li>
+                    <li className='listItem'>test</li>
+                </ul>
+            </div>
+
+            <button className='btn flex btnOne'>Contact</button>
+            {/ <button className='btn flex btnTwo'>Contact</button> */
+}
+
+// <div className="toggleIcon">
+{
+  /* <CgMenuGridO />
+            </div>
+        </div>
+    </div>
+  );
+} */
+}
+
+{
+  /* // export default Navbar */
+}
---
+++ src/Components/Subscribers/Subscribers.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Subscribers = () => {
+  return <div>Subscribers</div>;
+};
+
+export default Subscribers;
